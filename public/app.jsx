7cd881010dd2f910b8b1ac26d06f9b9093dbd99d
Alistair Willis <alistairrwillis@gmail.com>
2017-01-14 22:30:49 +0900

    Project complete

--- public/app.jsx
+++ public/app.jsx
@@ -1,83 +1,9 @@
-var GreeterMessage = React.createClass({
-    render: function () {
-        var name = this.props.name; 
-        var message = this.props.message; 
-        return (
-            <div>
-                <h1>Hello {name}!</h1>
-                <p>{message}</p> 
-            </div>
-        ); 
-    }
-});
+var React = require('react'); 
+var ReactDOM = require('react-dom');
+var Greeter = require('Greeter'); 
 
-var GreeterForm = React.createClass({
-    onFormSubmit: function (e) {
-        e.preventDefault(); 
 
-        var name = this.refs.name.value; 
-        var message = this.refs.message.value; 
-        var updates = {}; 
-
-        if (name.length > 0) {
-            this.refs.name.value = ''; 
-            updates.name = name; 
-        }
-
-        if (message.length > 0) {
-            this.refs.message.value = ''; 
-            updates.message = message; 
-        }
-
-        this.props.onNewData(updates);
-    },
-    render: function () {
-        return (
-            <form onSubmit={this.onFormSubmit}>
-            <div>
-                <input type="text" ref="name" />
-            </div> 
-            <div>
-                <textarea ref="message"></textarea>
-            </div>
-            <div>
-                <button>Submit</button>
-            </div>
-            </form>         
-        );
-    }
-});
-
-var Greeter = React.createClass({
-    getDefaultProps: function () {
-        return {
-            name: 'React', 
-            message: 'Component message default'
-        };
-    },
-    getInitialState: function () {
-        return {
-            name: this.props.name, 
-            message: this.props.message
-        }
-    },
-    handleNewData: function (updates) {
-        this.setState(updates);
-    },
-    render: function () {
-        var name = this.state.name;
-        var message = this.state.message;  
-
-        return (
-            <div>
-                <GreeterMessage name={name} message={message}/> 
-                <GreeterForm onNewData={this.handleNewData} />
-            </div>
-        );
-    }
-});
-
-var firstName = 'Genius Programmer'; 
+var firstName = 'Brilliant Programmer'; 
 
 ReactDOM.render(
     <Greeter name={firstName}/>,    
